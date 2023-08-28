b56d3ccecbd539f98963b069cc0f11b0c5b22f67
alejorcoder <129014518+alejorcode@users.noreply.github.com>
2023-08-28 10:53:07 -0300

    keycloak

--- gateway-service/src/main/java/com/gateway/service/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/gateway/service/config/SecurityConfig.java
@@ -7,15 +7,15 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
-                .csrf().disable()
-                .authorizeExchange().anyExchange().authenticated()
-                .and()
-                .oauth2Login();
+	  @Bean
+	  SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+	        http
+	                .csrf(csrf -> csrf.disable())
+	                .authorizeExchange().anyExchange().authenticated()
+	                .and()
+	                .oauth2Login();
 
-        return http.build();
-    }
+	        return http.build();
+	    }
 
 }
